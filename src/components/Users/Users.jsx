a10685ebb38df2bd9c38a8bf322c2df2901ea65c
Gosha Gaponov <egoriks1994@gmail.com>
2022-05-30 00:08:44 +0200

    refactor: func component to class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,125 @@
+import * as axios from "axios";
 import React from "react";
+import userPhoto from "../../assets/img/user.png";
 
-let Users = (props) => {
-    return (
-        <div>
-            {props.users.map((u) => (
-                <div key={u.id}>
-                    <span>
-                        {u.followed ? (
-                            <button
-                                onClick={() => {
-                                    props.unfollow(u.id);
-                                }}
-                            >
-                                Unfollow
-                            </button>
-                        ) : (
-                            <button
-                                onClick={() => {
-                                    props.follow(u.id);
-                                }}
-                            >
-                                Follow
-                            </button>
-                        )}
-                    </span>
-                    <span>
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        axios
+            .get("https://social-network.samuraijs.com/api/1.0/users")
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.users.map((u) => (
+                    <div key={u.id}>
                         <span>
-                            <div>{u.fullName}</div>
-                            <div>{u.status}</div>
+                            <img
+                                src={
+                                    u.photos.small != null
+                                        ? u.photos.small
+                                        : userPhoto
+                                }
+                            />
                         </span>
                         <span>
-                            <div>{u.location.city}</div>
-                            <div>{u.location.country}</div>
+                            {u.followed ? (
+                                <button
+                                    onClick={() => {
+                                        this.props.unfollow(u.id);
+                                    }}
+                                >
+                                    Unfollow
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={() => {
+                                        this.props.follow(u.id);
+                                    }}
+                                >
+                                    Follow
+                                </button>
+                            )}
                         </span>
-                    </span>
-                </div>
-            ))}
-        </div>
-    );
-};
+                        <span>
+                            <span>
+                                <div>{u.name}</div>
+                                <div>{u.status}</div>
+                            </span>
+                            <span>
+                                <div>{"u.location.city"}</div>
+                                <div>{"u.location.country"}</div>
+                            </span>
+                        </span>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+// let Users = (props) => {
+//     let getUsers = () => {
+//         if (props.users.length === 0) {
+//             axios
+//                 .get("https://social-network.samuraijs.com/api/1.0/users")
+//                 .then((response) => {
+//                     props.setUsers(response.data.items);
+//                 });
+//         }
+//     };
+
+//     return (
+//         <div>
+//             <button onClick={getUsers}>Get Users</button>
+
+//             {props.users.map((u) => (
+//                 <div key={u.id}>
+//                     <span>
+//                         <img
+//                             src={
+//                                 u.photos.small != null
+//                                     ? u.photos.small
+//                                     : userPhoto
+//                             }
+//                         />
+//                     </span>
+//                     <span>
+//                         {u.followed ? (
+//                             <button
+//                                 onClick={() => {
+//                                     props.unfollow(u.id);
+//                                 }}
+//                             >
+//                                 Unfollow
+//                             </button>
+//                         ) : (
+//                             <button
+//                                 onClick={() => {
+//                                     props.follow(u.id);
+//                                 }}
+//                             >
+//                                 Follow
+//                             </button>
+//                         )}
+//                     </span>
+//                     <span>
+//                         <span>
+//                             <div>{u.name}</div>
+//                             <div>{u.status}</div>
+//                         </span>
+//                         <span>
+//                             <div>{"u.location.city"}</div>
+//                             <div>{"u.location.country"}</div>
+//                         </span>
+//                     </span>
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// };
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -2,36 +2,7 @@ const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 let initialState = {
-    users: [
-        {
-            id: 1,
-            followed: false,
-            fullName: "Pupkin Vasya",
-            status: "I`m a boss",
-            location: { city: "Pupkovshina", country: "Zimbabve" },
-        },
-        {
-            id: 2,
-            followed: false,
-            fullName: "Zalupkin Petro",
-            status: "Hey brother",
-            location: { city: "Zalupka", country: "Zaluposransk" },
-        },
-        {
-            id: 3,
-            followed: true,
-            fullName: "Vova Da",
-            status: "Doyou or no doyou",
-            location: { city: "Kyiv", country: "Ukraine" },
-        },
-        {
-            id: 4,
-            followed: false,
-            fullName: "Dima No",
-            status: "No",
-            location: { city: "Muxosransk", country: "Pomoyka" },
-        },
-    ],
+    users: [],
 };
 
 const usersReducer = (state = initialState, action) => {
