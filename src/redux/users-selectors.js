4090ed1d9e0f14a1c162603112d892a2be882b25
Gosha Gaponov <egoriks1994@gmail.com>
2022-06-14 00:34:04 +0300

    refactor: refactor users-selectors

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,25 +1,27 @@
+import { createSelector } from "reselect";
+
 export const getUsersSelector = (state) => {
-        return state.usersPage.users
-
+    return state.usersPage.users;
 };
+// создавая селектор через либу надо вызвать createSelector и
+// передать первым аргументом простой селектор(зависимость) и
+// последним аргументом то куда результат сохранится в данном случае
+// в users 
+export const superSelector = createSelector(getUsersSelector, (users) => {
+    return users.filter((u) => true);
+});
 export const getPageSizeSelector = (state) => {
-        return state.usersPage.pageSize
-
+    return state.usersPage.pageSize;
 };
 export const getUsersCountSelector = (state) => {
-        return state.usersPage.totalUsersCount
-
+    return state.usersPage.totalUsersCount;
 };
 export const getCurrentPageSelector = (state) => {
-        return state.usersPage.currentPage
+    return state.usersPage.currentPage;
 };
 export const getIsFetchingSelector = (state) => {
-        return state.usersPage.isFetching
-
+    return state.usersPage.isFetching;
 };
 export const getIsFollowingSelector = (state) => {
-        return state.usersPage.isFollowing
-
+    return state.usersPage.isFollowing;
 };
-
-
