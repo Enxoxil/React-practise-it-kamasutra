7bdc40324b8593ead72ca17771194b5ce1d7a3d9
Gosha Gaponov <egoriks1994@gmail.com>
2022-06-16 00:01:21 +0300

    feat: create useEffect

--- src/components/Profile/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileStatusWithHooks.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 const ProfileStatusWithHooks = (props) => {
     const [editMode, setEditMode] = useState(false);
     const [status, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
+
     const activateMode = () => {
         setEditMode(true);
     };
