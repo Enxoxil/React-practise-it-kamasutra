0abf03d5034bd8f9c9414141d93a3f0a9651cd70
Gosha Gaponov <egoriks1994@gmail.com>
2022-06-22 00:38:19 +0300

    Fix: Use HashRouter for GH pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./components/App/App";
-import {Provider} from "react-redux"
-import store from './redux/redux-store'
+import { Provider } from "react-redux";
+import store from "./redux/redux-store";
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
+        {/* basename={process.env.PUBLIC_URL} -  УРЛ из окружения, HashRouter для работы с УРЛ с Хистори АПИ только он работает с гитхаб страницей*/}
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById("root")
 );
